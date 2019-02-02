4047ac15562bbc5578a53bb572191f7c65637bb6
yjlo123 <liusiwei.yjlo@gmail.com>
2019-02-02 17:43:15 +0800

    Load source code

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ let finished = false;
 
 let keys = [];
 
+
+/* Console */
+
 let jqconsole = $('#console').jqconsole();
 jqconsole.Write('Runtime Script\n', 'console-gray');
 jqconsole.SetPromptLabel('  ');
@@ -23,10 +26,52 @@ jqconsole.SetPromptLabel('  ');
 let startPrompt = function () {
 	// Start the prompt with history enabled.
 	jqconsole.Prompt(true, function (input) {
+		switch (input) {
+			case 'clear':
+			jqconsole.Reset();
+			break;
+		}
 		startPrompt();
 	});
 }
 startPrompt();
+
+/* Load Code */
+function getCodeUrl(codeId) {
+	return 'https://raw.githubusercontent.com/yjlo123/runtime-script/master/examples/' + codeId + '.runtime';
+}
+
+function getURLParameter(sParam) {
+	var sPageURL = window.location.search.substring(1);
+	var sURLVariables = sPageURL.split('&');
+	for (var i = 0; i < sURLVariables.length; i++) 
+	{
+		var sParameterName = sURLVariables[i].split('=');
+		if (sParameterName[0] == sParam) 
+		{
+			return sParameterName[1];
+		}
+	}
+}
+
+function getCode(codeId) {
+	$.ajax(getCodeUrl(codeId))
+	.done(function(code) {
+		editor.setValue(code);
+		editor.gotoLine(0);
+	})
+	.fail(function() {
+		jqconsole.Write('Loading source failed.\n', 'console-error');
+	})
+	.always(function() {
+		console.log('Load source code finished.')
+	});
+}
+
+let codeId = getURLParameter('src');
+if (codeId) {
+	getCode(codeId);
+}
 
 
 /* listener */
